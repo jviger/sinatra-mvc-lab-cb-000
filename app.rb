602784b8120baa46a0a55d30ca7e14af8bdd6448
Jason Viger <jason.viger@gmail.com>
2018-08-28 08:42:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,14 +4,12 @@ require_relative 'models/piglatinizer.rb'
 class App < Sinatra::Base
   
   get "/" do
-    
     erb :user_input
   end
   
-  post "/user_input"
+  post "/user_input" do
   
   @text = PigLatinizer.new(params[:user_text]).latinize
-  
   erb :pig_latinized
   end
   
