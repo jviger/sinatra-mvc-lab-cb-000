60d0c60f88b7f8c0a51e3811b76b67e2c96e9776
Jason Viger <jason.viger@gmail.com>
2018-08-28 09:40:56 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -26,7 +26,7 @@ class PigLatinizer
    def piglatinize_sentence(user_phrase)
     
       word_array = user_phrase.split(" ")
-      word_array.collect { piglatinize_word(word) }
+      word_array.collect { |x| piglatinize_word(x) }
       word_array.join(" ")
      
    end 
